d92f45c60cf33af486f48fa5ccc22603afe12efe
jiaqixu1 <e1355232@u.nus.edu>
2026-02-02 07:13:03 +0800

    Level-3: add Task class and support mark/unmark

--- src/main/java/Nock.java
+++ src/main/java/Nock.java
@@ -4,7 +4,7 @@ public class Nock {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm Nock");
@@ -19,18 +19,47 @@ public class Nock {
             }
 
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i].toDisplayString());
                 }
                 continue;
             }
 
-            // Any other input: store as a task
-            tasks[taskCount] = input;
+            if (input.startsWith("mark ")) {
+                int index = parseTaskNumber(input.substring(5)) - 1;
+                if (isValidIndex(index, taskCount)) {
+                    tasks[index].markDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[index].toDisplayString());
+                }
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                int index = parseTaskNumber(input.substring(7)) - 1;
+                if (isValidIndex(index, taskCount)) {
+                    tasks[index].markUndone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[index].toDisplayString());
+                }
+                continue;
+            }
+
+            // Otherwise: treat input as a new task
+            tasks[taskCount] = new Task(input);
             taskCount++;
             System.out.println("added: " + input);
         }
 
         scanner.close();
+    }
+
+    private static int parseTaskNumber(String s) {
+        return Integer.parseInt(s.trim());
+    }
+
+    private static boolean isValidIndex(int index, int taskCount) {
+        return index >= 0 && index < taskCount;
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    public String toDisplayString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
+
