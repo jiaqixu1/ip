96469be5e2662e5191735c0db8b8415d41b24e2c
jiaqixu1 <e1355232@u.nus.edu>
2026-02-20 20:55:07 +0800

    Extract TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public void add(Task t) {
+        tasks.add(t);
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public Task remove(int index) {
+        return tasks.remove(index);
+    }
+}
