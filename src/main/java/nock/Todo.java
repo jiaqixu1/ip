b7b03b19263c4af707e689947a137f96b8d5d911
jiaqixu1 <e1355232@u.nus.edu>
2026-02-26 23:00:46 +0800

    Add a Javadoc

--- src/main/java/nock/Todo.java
+++ src/main/java/nock/Todo.java
@@ -1,13 +1,14 @@
 package nock;
 
-/**
- * todo task
- */
 public class Todo extends Task {
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Print the todo string
+     * @return todo string
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
